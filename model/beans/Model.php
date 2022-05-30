179ed8fb0922add0ea7ea9f7db9ad5d934be5851
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-05-30 10:57:39 +0100

    Refactorisation du model et suppression des models pays et ville

--- model/beans/Model.php
+++ model/beans/Model.php
@@ -1,21 +1,25 @@
 <?php
 //require_once ('connexion.php');
 
-abstract class Model
-{
-    public static $connexion;
-    public static $dns = 'mysql:host=localhost;dbname=projetweb';
-    public static string $username = 'root';
-    public static $password = 'momo';
+    abstract class Model
+    {
+        public static $connexion;
+        public static $dns = 'mysql:host=localhost;dbname=projetweb';
+        public static string $username = 'root';
+        public static $password = 'momo';
 
-    public static function getConnexion(){
-        self::$connexion = null;
-        try{
-            self::$connexion = new PDO(self::$dns,self::$username,self::$password);
-            self::$connexion->exec('set names utf8');
-            //echo 'Connexion etablie';
-        }catch (PDOException $e){echo "ERREUR : ".$e->getMessage();}
+        public static function getConnexion()
+        {
+            self::$connexion = null;
+            try {
+                self::$connexion = new PDO(self::$dns, self::$username, self::$password);
+                self::$connexion->exec('set names utf8');
+                //echo 'Connexion etablie';
+            } catch (PDOException $e) {
+                echo "ERREUR : " . $e->getMessage();
+            }
 
+        }
     }
-}
+
 ?>
--- model/beans/Module.php
+++ model/beans/Module.php
@@ -1,12 +1,15 @@
 <?php
-require_once ('Model.php');
-class Module extends Model
-{
-    public $filiere,$niveau,$matP,$nom;
-    public function __construct($filiere,$niveau,$matP,$nom){
-        $this->filiere = $filiere;
-        $this->niveau = $niveau;
-        $this->matP = $matP;
-        $this->nom = $nom;
+    require_once('Model.php');
+
+    class Module extends Model
+    {
+        public $filiere, $niveau, $matP, $nom;
+
+        public function __construct($filiere, $niveau, $matP, $nom)
+        {
+            $this->filiere = $filiere;
+            $this->niveau = $niveau;
+            $this->matP = $matP;
+            $this->nom = $nom;
+        }
     }
-}
