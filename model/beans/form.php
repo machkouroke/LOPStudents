5c253fb22db242a834aaff854688c81286159c98
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-06-02 14:58:21 +0100

    -Ajout des constantes réprésentants les répertoires média Photo et CV -Quelque Refactorisation dans le code Form

--- model/beans/form.php
+++ model/beans/form.php
@@ -1,27 +1,22 @@
 <?php
-    if(isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['login'])
-                && isset($_POST['email']) && isset($_FILES['photo'])
-                && isset($_FILES['cv']) && isset($_POST['country']) && isset($_POST['city']) && isset($_POST['zipCode'])
-        && isset($_POST['faculty'])){
+    if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['login'])
+        && isset($_POST['email']) && isset($_FILES['photo'])
+        && isset($_FILES['cv']) && isset($_POST['country']) && isset($_POST['city']) && isset($_POST['zipCode'])
+        && isset($_POST['faculty'])) {
 
-        if(strlen($_POST['nom'])>20 ||
-            strlen($_POST['login'])>15 || strlen($_POST['password'])>8){
+        if (strlen($_POST['nom']) > 20 ||
+            strlen($_POST['login']) > 15 || strlen($_POST['password']) > 8) {
             throw new LenException('La taille des elements ne doit pas depasser 20 lettres');
         }
-        if(filesize($_FILES['photo'])>2 && filesize($_POST['cv'])>2){
+        if (filesize($_FILES['photo']) > 2 && filesize($_POST['cv']) > 2) {
             throw new SizeException('La taille des fichiers ne doit pas depasser ne doit pas depasser 2 MO');
-        }else{
+        } else {
             $login = $_POST['login'];
-            $cvTmpName = $_FILES['cv']['tmp_name'];
-            $photoTmpName = $_FILES['photo']['tmp_name'];
-            $cvfolder = 'cv/';
-            $photoFolder = 'photos/';
-            $_POST['cv'] = "$cvfolder/$login";
-            $_POST['photo'] = "$photoFolder/$login";
-            move_uploaded_file($cvTmpName,$_POST['cv']);
-            move_uploaded_file($photoTmpName,$_POST['photo']);
+            $_POST['cv'] = CV_DIR . DIRECTORY_SEPARATOR . $login;
+            $_POST['photo'] = PIC_DIR . DIRECTORY_SEPARATOR . $login;
+            move_uploaded_file($_FILES['photo']['tmp_name'], $_POST['cv']);
+            move_uploaded_file($_FILES['cv']['tmp_name'], $_POST['photo']);
         }
-    }else{
+    } else {
         echo "Tous les champs ne sont remplis";
     }
-?>
