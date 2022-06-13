a2e1359e8ef3908b751b985429c6d387443f2083
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-06-13 17:47:26 +0100

    Modification de l'affichage des utillisateurs par leur id d'ajout

--- model/LOPStudents/Student.php
+++ model/LOPStudents/Student.php
@@ -76,7 +76,7 @@
         public static function getAll(int $first, int $last): bool|array
         {
             $con = FACTORY->get_connexion();
-            $sql = 'SELECT * FROM etudiants NATURAL JOIN users ORDER BY NAME  LIMIT ' . $first . ',' . $last;
+            $sql = 'SELECT * FROM etudiants NATURAL JOIN users ORDER BY ID  desc LIMIT ' . $first . ',' . $last;
             $res = $con->query($sql);
             return self::changeToStudent($res);
         }
